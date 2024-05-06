ccf7fcf74dd37bd6051e10e4edf01c9b6ce474ba
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-05-06 23:06:57 +0530

    first basic project

---
+++ project1/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+
+const Hero = () => {
+    return(
+        <div className="container">
+        <div className="second-container">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE <br/>THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <button>Shop Now</button>
+            <button  className="btn-transparent">Category</button>
+            <p>Also Available On</p>
+            <div className="bottom-img">
+            <img src="/images/flipkart.png" alt="error" />
+            <img src="/images/amazon.png" alt="error" />
+            </div>
+        </div>
+        <div className="hero-img">
+            <img src="/images/shoe_image.png" alt="" />
+        </div>
+        </div>
+        </div>
+    )
+}
+
+export default Hero
---
+++ project1/src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+    return(
+        <nav>
+          <div className='logo'>
+          <img src="/images/brand_logo.png" alt="Error" />
+          </div>
+
+          <ul>
+            <li>MENU</li>
+            <li>LOCATION</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+          </ul>
+          <button>Login</button>
+        </nav>
+    )
+};
+
+export default Navigation;
