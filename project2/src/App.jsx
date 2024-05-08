48f2d76a14ff537908150814470d32791bc16571
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-05-09 00:31:22 +0530

    props

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,10 +1,20 @@
 
 import './App.css'
 import Navigation from './components/Navigation/Navigation2'
+import Hero from './components/Hero/Hero'
+import Button from './components/Button/Button'
 
 function App() {
   return (
+    <>
    <Navigation/>
+   <Hero/>
+   <Button text="VIA SUPPORT CHAT"/>
+   <Button text="VIA CALL"/>
+   <Button 
+   isOutline={true}
+   text="VIA EMAIL FORM"/>
+   </>
   )
 }
 
---
+++ project2/src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import styles from "./Button.module.css"
+
+const Button = (props) => {
+    console.log(props);
+    return(
+        <div className={`${styles.content_btn}`}>
+        <button className={props.isOutline ? styles.outline_btn : styles.primary_btn}>
+            {props.text}
+        </button>
+        </div> 
+    )
+}
+
+export default Button
