b81beb6d83523d8d6107e6c8028f136a5d8ec298
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-05-27 20:14:06 +0530

    react_router 1 way with basic project

---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import About from './components/About/About'
+import Layout from './components/Layout'
+import Home from './components/Home/Home'
+
+const router = createBrowserRouter(
+  [
+    {
+    path: '/',
+    element: <Layout/>,
+
+    children: [{
+
+      path: "",
+      element: <Home/>
+
+    },
+    {
+      path: "about",
+      element: <About/>
+    }
+  ]
+
+    }
+  ]
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
