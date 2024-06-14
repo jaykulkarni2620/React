e4de93f4a277042b33816607c400ded0e9e98ca0
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-06-15 01:03:05 +0530

    login & logout functionality done, add the tailwind and some components

--- 12MegaBlock/src/App.jsx
+++ 12MegaBlock/src/App.jsx
@@ -1,17 +1,51 @@
-
+import { useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
+import authService from './appwrite/authservice'
+import { login, logout} from './store/authSlice'
+import Header from '../src/components/Header/Header'
+import Footer from '../src/components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  
-console.log()
 
-  return (
-    <>
+  // loading state banvnar karan application data fetch karnar tar appwrite ko localstorage me to rakha nahi hai to uske liye time lagega, same for database or network madhun kahi gheyaych asel tar time lagto, tar ashya veli "conditional rendering karu shakto using if/else ke loading true ahe tr loading icon dakhvnar as"
 
-      <h1>A Blog With Appwrite</h1>
-      
-    </>
-  )
+  const [ loading, setLoading] = useState(true);
+
+  //for sending data in store 
+  const dispatch = useDispatch()
+
+  // checking the user log in or not using the useEffect()
+  //.then means if is run then what to do
+  // .finally:- .then ho .catch ho .finally hamesha run hota he hai
+
+  useEffect(() => {
+
+    authService.getCurrentUser()
+    // humlog ko data or userdata milta hai
+    .then( (userData) => {
+      if (userData) {
+        dispatch(login({ userData }))
+      } else {
+        dispatch(logout())
+      }
+    })
+    // setloading la false kel
+    .finally( ()=> setLoading(false))
+    
+  },[])
+
+  //frontend view reverse stament or condition means jar loading true asel tar false kar 
+  return !loading ? (<div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+  <div className="w-full">
+      <Header />
+      <main>
+      TODO{/* < Outlet /> */  }
+      </main>
+      <Footer />
+    </div>
+  </div>) : (null)
 }
 
 export default App
--- 12MegaBlock/src/appwrite/authservice.js
+++ 12MegaBlock/src/appwrite/authservice.js
@@ -70,4 +70,4 @@ const authService = new AuthService();
 
 
 
-export default AuthService
+export default authService
