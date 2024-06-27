0871b96ec65e798d84971725a81d09ab8fb2142d
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-06-27 18:26:05 +0530

    login function with added inputs

--- 12MegaBlock/src/components/Login.jsx
+++ 12MegaBlock/src/components/Login.jsx
@@ -9,19 +9,24 @@ import {useForm} from "react-hook-form"
 function Login () {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+
+    // in useForm the handleSumbit work as "Key" // also we use register here and the register automatically input madhun value ghenar also state handle kart aplyala karaychi garaj nahi
     const { register, handleSubmit} = useForm();
+
     const [error, setError] = useState("")
 
 
     const login = async(data) => {
+        console.log(data);
         setError("");
         try {
            const session = await authService.login(data);
+
            if (session) {
             const userData = await authService.getCurrentUser()
             if (userData) {
                 dispatch(authLogin(userData))
-                // if user login then route pe usko bhej do ( Link pe click karna padta hai or navigate me khudse redirect hota hai )
+                // if user login then route pe usko bhej do ( Link pe click karna padta hai or navigate me khudse redirect hota hai ) 
                 navigate('/')
             }
            }
@@ -33,8 +38,55 @@ function Login () {
 
 
     return(
-        <div>
-            Login
+        <div className=" flex items-center justify-center w-full">
+            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+                    <div className="mb-2 flex justify-center">
+                        <span className="inline-block w-full max-w-[100px]">
+                            <Logo width="100%" />
+                        </span>
+                    </div>
+            <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+            <p className="mt-2 text-center text-base text-black/60">
+            Don&apos;t have any account?&nbsp;
+                    <Link
+                        to="/signup"
+                        className="font-medium text-primary transition-all duration-200 hover:underline"
+                    >
+                        Sign Up
+                    </Link>
+            </p>
+
+            {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+                    <form onSubmit={handleSubmit(login)} className='mt-8'>
+                        <div className="space-y-5">
+                            <Input 
+                            label="Email:"
+                            placeholder="Enter Your email"
+                            type="email"
+                            // spread karna jaruri hai register ko warna overright hoga (key value )  and second object
+                            {...register("email", {
+                                required: true,
+                                validate: {
+                                    matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || "Email address must be a valid address",
+                                }
+                            })}
+                            />
+
+                            <Input
+                            label="password"
+                            placeholder= "Enter your password"
+                            type= "password"
+                            {...register("password", {
+                                required: true
+                            })}
+                            />
+                            <Button
+                            type="submit"
+                            className="w-full"
+                            >login</Button>
+                        </div>
+                    </form>
+            </div>
         </div>
     )
 }
